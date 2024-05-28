3e8eec7dc5df46d806f5b9ba1a5e8e7e33ced101
roshan3055 <81009695+roshan3055@users.noreply.github.com>
2024-05-28 16:02:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+//Below is the Demonstration of integrating API using rapidapi, Go to javascript, fetch and then copy the code. 
+//Make sure you link this Javascript file in the main index.htm inside <script> tag.
+
+
+
+/*const url = 'https://foreca-weather.p.rapidapi.com/location/search/mumbai?lang=en&country=in';
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '84f7fb3a32mshb38c3f15b3358a7p1daca1jsn5e3a077d26a2',
+		'X-RapidAPI-Host': 'foreca-weather.p.rapidapi.com'
+	}
+};
+
+(async ()=>{
+try {
+	const response = await fetch(url, options);
+	const result = await response.text();
+	console.log(result);
+} catch (error) {
+	console.error(error);
+}})()  */
